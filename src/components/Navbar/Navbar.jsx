67062d141717117a01e332f0cf3ef1a3a6fa1431
Surojit Majumdar <144248306+SurojIT610@users.noreply.github.com>
2024-09-03 16:39:21 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = ({ onSearch }) => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <h1 className="navbar-brand">
-          GutkaApp
+          Restro Guru
         </h1>
         <button
           className="navbar-toggler"
